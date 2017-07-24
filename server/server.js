a46985b069ec4156c5298b3a8814688e6993ec5a
Richard Turner <richardturnerdeveloper@gmail.com>
2017-07-24 16:03:26 -0400

    Fix issue with duplicate todos

--- server/server.js
+++ server/server.js
@@ -28,7 +28,9 @@ app.post("/todos", (req,res) => {
   });
   Todo.find({text: req.body.text}).then((foundTodo) => {
     if (foundTodo){
+      if (foundTodo === req.body.text){
       return res.send('Todo already exists!');
+      }
     }
   });
   todo.save().then((doc) => {
