5d6dae8dabb8e03423f9c6e84d1c2c024a480941
Richard Turner <richardturnerdeveloper@gmail.com>
2017-07-25 17:47:38 -0400

    Fix issue with heroku enviornment

--- server/config/config.js
+++ server/config/config.js
@@ -6,4 +6,6 @@ if (env === 'development'){
 } else if (env === 'test'){
   process.env.PORT = 3000;
   process.env.MONGO_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if (env === 'production'){
+  process.env.MONGO_URI = "mongodb://admin:password123@ds151008.mlab.com:51008/todoapp";
 }
